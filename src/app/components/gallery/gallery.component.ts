f554f8d89bd5b623bab82117b42ef7e1c135129b
Dagbfatih <Dagbfatih@users.noreply.github.com>
2022-05-04 00:33:39 +0300

    last commit 8

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { allTranslates } from 'src/app/services/translation.service';
 import { Mold } from 'src/app/models/entities/mold';
 import { MoldService } from 'src/app/services/mold.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-gallery',
@@ -14,6 +15,7 @@ export class GalleryComponent implements OnInit {
 
   constructor(
     private moldService: MoldService,
+    private modalService: NgbModal
   ) {}
 
   ngOnInit(): void {
@@ -21,8 +23,8 @@ export class GalleryComponent implements OnInit {
   }
 
   open(path: string, content: any) {
-    // this.currentPhotoPath = path;
-    // this.modalService.open(content, { centered: true, size: 'xl' });
+    this.currentPhotoPath = path;
+    this.modalService.open(content, { centered: true, size: 'xl' });
   }
 
   getAllMolds() {
